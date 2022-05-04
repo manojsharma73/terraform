10cd7272deb0cc6832dbb052c3f3133344bd68d1
manojsharma73 <48566618+manojsharma73@users.noreply.github.com>
2022-05-04 08:36:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,6 +93,20 @@ resource "aws_instance" "webserver1" {
 
 }
 
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0d5eff06f840b45e9"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-2.id
+  user_data              = file("install_apache.sh")
+
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
 # Create Web Security Group
 resource "aws_security_group" "web-sg" {
   name        = "Web-SG"
@@ -124,7 +138,7 @@ resource "aws_lb" "external-elb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web-sg.id]
-  subnets            = [aws_subnet.web-subnet-1.id]
+  subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
 }
 
 resource "aws_lb_target_group" "external-elb" {
@@ -141,6 +155,16 @@ resource "aws_lb_target_group_attachment" "external-elb1" {
 
   depends_on = [
     aws_instance.webserver1,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "external-elb2" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.webserver2,
   ]
 }
 
