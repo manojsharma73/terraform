4dca1fe9abc602ee42ebab23ca1b17bfddf35a23
manojsharma73 <48566618+manojsharma73@users.noreply.github.com>
2022-05-04 07:11:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "webserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
   user_data              = file("install_apache.sh")
 
